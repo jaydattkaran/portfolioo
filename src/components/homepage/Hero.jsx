16504d5bb1ef5229f450d91aa207d131e852ee63
jaydattkaran <karanjaydatt03@gmail.com>
2024-12-22 18:20:24 +0530

    vanta effect added

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -1,13 +1,36 @@
-import ParticlesCanvas from "./ParticleCanvas";
-import { useEffect, useRef } from "react";
+// import ParticlesCanvas from "./ParticleCanvas";
+import React, { useState, useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import WebFont from 'webfontloader';
-{/* <style>
-@import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap');
-</style> */}
+import BIRDS from 'vanta/dist/vanta.birds.min'
+import * as THREE from 'three';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-export default function Hero() {
+export default function Hero(props) {
+
+  const [vantaEffect, setVantaEffect] = useState(null)
+  const myRef = useRef(null)
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(BIRDS({
+        el: myRef.current,
+        THREE: THREE,
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        backgroundColor: 0xffffff,
+        color1: 0xeb1111
+      }))
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy()
+    }
+  }, [vantaEffect])
+
 
   const cta = useRef(null);
   const tl = gsap.timeline();
@@ -30,9 +53,8 @@ export default function Hero() {
   }, []);
 
   return (
-    <div className="z-0 relative w-full h-screen flex items-center justify-center bg-secondary-100 overflow-hidden">
+    <div className="z-0 relative w-full h-screen flex items-center justify-center bg-secondary-100 overflow-hidden" ref={myRef}>
       {/* Particle Background */}
-      <ParticlesCanvas />
 
       {/* Hero Content */}
       <div className="relative z-10 text-center">
@@ -42,7 +64,7 @@ export default function Hero() {
         <h1 className="text-black text-[4rem] md:text-[10rem] lg:text-[10rem] font-bold leading-tight mb-6">
           Digital Designer <br /> Creative Developer
         </h1>
-        <p style={{fontFamily:'Caveat'}} className="text-black text-[1.5rem] font-medium mb-6">
+        <p style={{ fontFamily: 'Caveat' }} className="text-black text-[1.5rem] font-medium mb-6">
           A digital designer and creative developer from India passionate about creating memorable experiences.
         </p>
         <a
